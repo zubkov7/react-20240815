10862b35a0e0ea702fa916b42956c11564a70c94
Andrey Zubkov <zubkov.andrey.99@mail.ru>
2024-08-21 18:15:03 +0300

    hw-2

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
-import { HeadphonePage } from "./components/headphone-page/headphone-page";
+import { HeadphonesPage } from "./components/headphones-page/headphones-page";
 import { Layout } from "./components/layout/layout";
 
 export const App = () => {
   return (
     <>
       <Layout>
-        <HeadphonePage title='headphones app' />
+        <HeadphonesPage title='headphones app' />
       </Layout>
     </>
   );
